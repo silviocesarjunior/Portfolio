af20f2f6813a92ed63615809ff2b111921dd6058
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2021-12-26 17:39:54 -0300

    Adicionando slick
    slick nos depoimentos - em construção

--- Projeto/main.js
+++ Projeto/main.js
@@ -1,9 +1,24 @@
-let botao = document.querySelector('.menu-principal__btn');
-let menuPrincipal = document.querySelector('.menu-principal');
+// Javascript puro
+// let botao = document.querySelector('.menu-principal__btn');
+// let menuPrincipal = document.querySelector('.menu-principal');
 
-botao.addEventListener("click", abreFechaMenu);
+// botao.addEventListener("click", abreFechaMenu);
 
-function abreFechaMenu(evento){
-    menuPrincipal.classList.toggle('menu-principal--fechado');
-    
-}
+// function abreFechaMenu(evento){
+//    menuPrincipal.classList.toggle('menu-principal--fechado');
+// }
+
+// jquery
+$(document).ready(function(){
+    // Botão do menu
+    $('.menu-principal__btn').click(function(){
+        $('.menu-principal').toggleClass('menu-principal--fechado');
+    });
+
+    // Slide depoimentos
+    $('.depoimentos__caixa').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
+});
